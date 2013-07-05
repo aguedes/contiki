3baaf408a3bf1a7aeb4eb51b02981741351d847e
Andre Guedes <andre.guedes@openbossa.org>
2013-07-05 19:52:30 -0300

    cc2540mini: Initial support for CC2540 mini development kit
    This patch add the initial files for supporting CC2540 mini
    development kit from TI.
    
    Signed-off-by: Andre Guedes <andre.guedes@openbossa.org>

---
+++ platform/cc2540mini/contiki-conf.h
@@ -0,0 +1,14 @@
+#ifndef __CONTIKI_CONF_H__
+#define __CONTIKI_CONF_H__
+
+#include <stdint.h>
+#include <string.h>
+
+/* Define some platform related type */
+typedef unsigned short clock_time_t;
+typedef unsigned short uip_stats_t;
+
+#define CCIF
+#define CLIF
+
+#endif /* __CONTIKI_CONF_H__ */
