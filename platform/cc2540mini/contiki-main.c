9f75224789788a65ca2bff5d148d91e8184ce3df
Paulo Borges <paulo.borges@openbossa.org>
2013-07-12 17:24:04 -0300

    cc2540mini: Initialize clock module
    Since cc2540 and cc253x shares same SFR for clock configuration, we can safely
    call cc253x's implementation. We also need to call soc_init() because it
    activates the global interruption flag (EA).
    
    Signed-off-by: Paulo Borges <paulo.borges@openbossa.org>

--- platform/cc2540mini/contiki-main.c
+++ platform/cc2540mini/contiki-main.c
@@ -1,5 +1,8 @@
 #include "8051def.h"
+#include "soc.h"
+#include "sys/clock.h"
 #include "dev/leds.h"
+#include "dev/clock-isr.h"
 
 int
 main(void) CC_NON_BANKED
@@ -7,6 +10,9 @@ main(void) CC_NON_BANKED
   leds_init();
 
   leds_off(LEDS_ALL);
+
+  clock_init();
+  soc_init();
 
   return 0;
 }
